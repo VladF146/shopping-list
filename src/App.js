e1da6c078ac7874292393572e9ba5a505525404e
Vladislav Ustinkin <vlad.fast146@gmail.com>
2022-10-07 12:40:17 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef } from "react";
+import { search } from "fast-fuzzy";
+import "./App.css";
 
 function App() {
+  const [shoppingList, setShoppingList] = useState([]);
+  const [itemsToChooseFrom, setItemsToChooseFrom] = useState([]);
+  const [chosenItems, setChosenItems] = useState([]);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await fetch(
+        "https://fetch-me.vercel.app/api/shopping/items"
+      );
+      response = await response.json();
+      const onlyItemNames = response.data.map((e) => {
+        return e.name.de;
+      });
+      setShoppingList(onlyItemNames);
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    searchFuzzily(inputRef.current.value);
+  }, [shoppingList]);
+
+  function searchFuzzily(value) {
+    setItemsToChooseFrom(search(value, shoppingList));
+  }
+
+  function addItem(value) {
+    setChosenItems([...chosenItems, value]);
+    setShoppingList(shoppingList.filter((e) => e !== value));
+  }
+
+  function removeItem(value) {
+    setChosenItems(chosenItems.filter((e) => e !== value));
+    setShoppingList([...shoppingList, value]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Shopping list with fuzzy search</h1>
+      <ul>
+        {chosenItems.map((e) => (
+          <li key={Math.random()}>
+            <button onClick={() => removeItem(e)}>{e}</button>
+          </li>
+        ))}
+      </ul>
+      <input
+        ref={inputRef}
+        onChange={(event) => searchFuzzily(event.target.value)}
+      />
+      <ul>
+        {itemsToChooseFrom.map((e) => (
+          <li key={Math.random()}>
+            <button onClick={() => addItem(e)}>{e}</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
