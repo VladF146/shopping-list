13ff5b50ec88da1704efd75652c7ba0b8c9a74eb
Vladislav Ustinkin <vlad.fast146@gmail.com>
2022-10-09 15:37:16 +0200

    debug

--- src/components/RecentItems.js
+++ src/components/RecentItems.js
@@ -5,40 +5,46 @@ export default function RecentItems({ removeRecent, recentItems, language }) {
   const [active, setActive] = useState(false);
   return (
     <>
-      <CollapsibleHead onClick={() => setActive(!active)}>
-        <h2>{language === "de" ? "Jüngste Artikel" : "Recent items"}</h2>
-        <span>
-          {active ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="currentColor"
-              className="bi bi-chevron-up"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="currentColor"
-              className="bi bi-chevron-down"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
-              />
-            </svg>
-          )}
-        </span>
-      </CollapsibleHead>
+      {recentItems.length > 0 ? (
+        <CollapsibleHead onClick={() => setActive(!active)}>
+          <h2>{`${language === "de" ? "Jüngste Artikel" : "Recent items"} (${
+            recentItems.length
+          })`}</h2>
+          <span>
+            {active ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                className="bi bi-chevron-up"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                className="bi bi-chevron-down"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                />
+              </svg>
+            )}
+          </span>
+        </CollapsibleHead>
+      ) : (
+        ""
+      )}
       {active ? (
         <ItemList>
           {recentItems.length > 0 &&
