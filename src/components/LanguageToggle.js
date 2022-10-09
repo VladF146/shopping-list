4f01deabbcfd49c56caac699b5abd9b684282bee
Vladislav Ustinkin <vlad.fast146@gmail.com>
2022-10-09 15:51:41 +0200

    better toggle button

--- src/components/LanguageToggle.js
+++ src/components/LanguageToggle.js
@@ -3,18 +3,27 @@ import styled from "styled-components";
 export default function LanguageToggle({ language, setLanguage }) {
   return (
     <ToggleContainer>
-      <ToggleButton onClick={() => setLanguage("de")}>
-        {language === "de" ? "Deutsch" : "German"}
-      </ToggleButton>
-      <ToggleButton onClick={() => setLanguage("en")}>
-        {language === "de" ? "Englisch" : "English"}
-      </ToggleButton>
+      {language === "en" ? (
+        <ToggleButton onClick={() => setLanguage("de")}>
+          {language === "de" ? "DE" : "GER"}
+        </ToggleButton>
+      ) : (
+        <ToggleButton onClick={() => setLanguage("en")}>
+          {language === "de" ? "EN" : "EN"}
+        </ToggleButton>
+      )}
     </ToggleContainer>
   );
 }
 
-const ToggleContainer = styled.div``;
+const ToggleContainer = styled.div`
+  border-radius: 15px;
+  overflow: hidden;
+`;
 
 const ToggleButton = styled.button`
+  width: 50px;
   padding: 10px;
+  border-style: none;
+  background-color: #a7f3d0;
 `;
